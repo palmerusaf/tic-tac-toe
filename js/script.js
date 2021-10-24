7e4156fbd36ae25d2dc3859478ca412f877652df
Branden Palmer <brandenpalmer08@gmail.com>
2021-10-24 18:40:11 +0700

    add funtions to BoardCell factory

--- js/script.js
+++ js/script.js
@@ -6,4 +6,15 @@ const BoardCell = function (rowIndex, columnIndex) {
   const _columnIndex = columnIndex;
   let _isPlayed = false;
   let _playersMark = "";
+  const getRowIndex = () => _rowIndex;
+  const getColumnIndex = () => _columnIndex;
+  const getIsPlayed = () => _isPlayed;
+  const setPlayersMark = function (mark) {
+    if (_isPlayed) return console.log("Cell already played.");
+    if (mark !== "X" && mark !== "O")
+      return console.log("Error: Invalid mark.");
+    _playersMark = mark;
+    _isPlayed = true;
+  };
+  const getPlayersMark = () => _playersMark;
 };
