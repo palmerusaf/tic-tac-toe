25952c8aaa6114a04e36cb7554e57df66c7f4f71
Branden Palmer <brandenpalmer08@gmail.com>
2021-10-24 18:37:40 +0700

    setup BoardCell factory boilerplate, add local variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// create game board cells for gameboard array
+const BoardCell = function (rowIndex, columnIndex) {
+  const _rowIndex = rowIndex;
+  const _columnIndex = columnIndex;
+  let _isPlayed = false;
+  let _playersMark = "";
+};
